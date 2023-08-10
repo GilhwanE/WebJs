08dd4d3ad1ef736c29786b222623a1a836bf1607
gilhwan <dyghks123@gmail.com>
2023-08-10 17:27:25 +0900

    ✨ Add quote localstroage Date func

--- js/quote.js
+++ js/quote.js
@@ -1,24 +1,38 @@
 const quoteElement = document.querySelector('.quote-contents');
 
-//명언 결과값 화면에 출력
+const quoteItem = localStorage.getItem('quote');
+const nowDate = new Date();
+const month = nowDate.getMonth() + 1;
+const date = nowDate.getDate();
 
+//명언 결과값 화면에 출력
 const quoteView = (result) => {
+  let quote = { createDate: `${month}-${date}`, quoteData: result };
+  localStorage.setItem('quote', JSON.stringify(quote));
   quoteElement.textContent = result;
 };
 
 const quoteAPI = async () => {
   try {
-    const data = await fetch('https://random-quote.hyobb.com/').then((res) => {
+    const data = await fetch(`https://random-quote.hyobb.com/`).then((res) => {
       res.json();
     });
     const result = data[1].respond;
     quoteView(result);
+    console.log(result);
   } catch (err) {
     console.log(`error:${err}`);
     quoteView('명언이 들어간다');
   }
 };
 
-quoteAPI();
+if (quoteItem) {
+  let { createDate, quoteData } = JSON.parse(quoteItem);
+  if (createDate === `${month}-${date}`) {
+    quoteElement.textContent = `"${quoteData}"`;
+  }
+} else {
+  quoteAPI();
+}
 
 // fetch 함수는 비동기 함수이다. 따라서 async await 키워드가 따라와야한다.
